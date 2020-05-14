cdc27812f9431c39189ccf5a15bc4ac6e7e7443a
Luz Perdomo <lilyperdomo92@gmail.com>
2020-05-14 15:47:06 +0000

    fixed betty

--- maths.c
+++ maths.c
@@ -42,27 +42,27 @@ void sub(stack_t **stack, unsigned int line_number)
 	stack_t *temp = *stack;
 	int sub;
 
-    if (temp == NULL || temp->next == NULL)
-    {
-        printf("L%u: can't sub, stack too short\n", line_number);
+	if (temp == NULL || temp->next == NULL)
+	{
+		printf("L%u: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
-    }
+	}
 
-    while (temp->next != NULL)
+	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
-	
+
 	sub = temp->n - temp->prev->n;
 	temp->prev->n = sub;
-    temp->prev->next = NULL;
+	temp->prev->next = NULL;
 
 	free(temp);
 }
 
 
 /**
- *div - divide two values of the top of stack
+ *_div - divide two values of the top of stack
  *@stack: pointer to the top of the stack
  *@line_number: the line number of the command being run
  *
@@ -73,20 +73,20 @@ void _div(stack_t **stack, unsigned int line_number)
 	stack_t *temp = *stack;
 	int div;
 
-    if (temp == NULL || temp->next == NULL)
-    {
-        printf("L%u: can't div, stack too short\n", line_number);
+	if (temp == NULL || temp->next == NULL)
+	{
+		printf("L%u: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
-    }
+	}
 
-    while (temp->next != NULL)
+	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
-	
+
 	div = temp->n / temp->prev->n;
 	temp->prev->n = div;
-    temp->prev->next = NULL;
+	temp->prev->next = NULL;
 
 	free(temp);
 }
@@ -104,27 +104,27 @@ void mul(stack_t **stack, unsigned int line_number)
 	stack_t *temp = *stack;
 	int mul;
 
-    if (temp == NULL || temp->next == NULL)
-    {
-        printf("L%u: can't mul, stack too short\n", line_number);
+	if (temp == NULL || temp->next == NULL)
+	{
+		printf("L%u: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
-    }
+	}
 
-    while (temp->next != NULL)
+	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
-	
+
 	mul = temp->n * temp->prev->n;
 	temp->prev->n = mul;
-    temp->prev->next = NULL;
+	temp->prev->next = NULL;
 
 	free(temp);
 }
 
 
 /**
- *mod -   module of two values of the top of stack
+ *mod -module of two values of the top of stack
  *@stack: pointer to the top of the stack
  *@line_number: the line number of the command being run
  *
@@ -135,20 +135,20 @@ void mod(stack_t **stack, unsigned int line_number)
 	stack_t *temp = *stack;
 	int mod;
 
-    if (temp == NULL || temp->next == NULL)
-    {
-        printf("L%u: can't mod, stack too short\n", line_number);
+	if (temp == NULL || temp->next == NULL)
+	{
+		printf("L%u: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
-    }
+	}
 
-    while (temp->next != NULL)
+	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
-	
+
 	mod = temp->n % temp->prev->n;
 	temp->prev->n = mod;
-    temp->prev->next = NULL;
+	temp->prev->next = NULL;
 
 	free(temp);
 }
