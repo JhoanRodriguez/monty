3b79fdc8b5b79cbbd0acb29026406b709a9ed986
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-14 18:51:04 +0000

    hot fix

--- commands.c
+++ commands.c
@@ -16,10 +16,10 @@ void (*_commands(char *opcode, unsigned int ln))(stack_t **head, unsigned int)
 		{"swap", swap},
 		{"add", add},
 		{"nop", nop},
-		{"nop", sub},
-		{"nop", _div},
-		{"nop", mul},
-		{"nop", mod},
+		{"sub", sub},
+		{"div", _div},
+		{"mul", mul},
+		{"mod", mod},
 		{NULL, _free}
 	};
 	unsigned int x = 0;
