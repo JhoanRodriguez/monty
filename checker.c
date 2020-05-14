72c6a63994be7cfc37911b58e4352c2213b78380
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-14 04:34:57 +0000

    fixing the else statment

--- checker.c
+++ checker.c
@@ -10,20 +10,19 @@
  */
 int checker(unsigned int line_number, char **commands, stack_t **stack)
 {
-	if (commands[1])
+	
+	if(is_number(commands[1]) == 1)
 	{
-		if(is_number(commands[1]) == 1)
-		{
 
-			glob_vars.glob_int = atoi(commands[1]);
-	    	get_commands(commands[0], line_number)(stack, line_number);
-		}
+		glob_vars.glob_int = atoi(commands[1]);
+		get_commands(commands[0], line_number)(stack, line_number);
 	}
 	else if (strcmp(commands[0], "push") == 0)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	else
 		get_commands(commands[0], line_number)(stack, line_number);
 	return (0);
 }
